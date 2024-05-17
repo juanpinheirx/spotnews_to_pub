f868a51074d49588c587ab21d3ff2f376532d28c
juanpinheirx <jroliveirapinheiro@gmail.com>
2024-05-16 21:18:52 -0300

    req 01 //2

--- news/models.py
+++ news/models.py
@@ -3,7 +3,7 @@ from utils.validators import validate_name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, validators=[validate_name])
+    name = models.CharField(max_length=200, validators=[validate_name])
 
     def __str__(self):
         return self.name
