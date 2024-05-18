985b70dc4cbe331afad8af6b4d6a32266fb2c2e9
juanpinheirx <jroliveirapinheiro@gmail.com>
2024-05-18 05:38:19 -0300

    req 06

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from news.views import details, home
+from news.views import categories_form, details, home
 
 
 urlpatterns = [
     path("http://127.0.0.1:8000", home, name="home-page"),
     path("news/<int:id>/", details, name="news-details-page"),
+    path("categories/", categories_form, name="categories-form"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
-from news.models import News
+from django.shortcuts import get_object_or_404, redirect, render
+from news.forms import CreateCategoryForm
+from news.models import Category, News
 
 
 # Create your views here.
@@ -12,3 +12,14 @@ def home(request):
 def details(request, id):
     news = {"news": get_object_or_404(News, id=id)}
     return render(request, "news_details.html", news)
+
+
+def categories_form(request):
+    form = CreateCategoryForm()
+    if request.method == "POST":
+        form = CreateCategoryForm(request.POST)
+        if form.is_valid():
+            Category.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+    context = {"form": form}
+    return render(request, "categories_form.html", context)
